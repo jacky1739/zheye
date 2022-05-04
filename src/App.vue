a74331cd63e014835160685d2d3b88d24d5248ba
Jacky <jacky123789456@gmail.com>
2022-05-04 14:07:59 +0800

    新增 columnlist.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的專欄',
+    description: '這是test1專欄, 有一段非常有意思的簡介, 可以更新一下歐',
+    avatar: 'https://avatars.githubusercontent.com/u/6128107?s=280&v=4'
+  },
+  {
+    id: 2,
+    title: 'test2的專欄',
+    description: '這是test2專欄, 有一段非常有意思的簡介, 可以更新一下歐',
+    avatar: 'https://avatars.githubusercontent.com/u/6128107?s=280&v=4'
+  },
+  {
+    id: 3,
+    title: 'test3的專欄',
+    description: '這是test3專欄, 有一段非常有意思的簡介, 可以更新一下歐',
+    avatar: 'https://avatars.githubusercontent.com/u/6128107?s=280&v=4'
+  }
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
