dff9495507b6a806b3048936d2d3d7b894296c5b
Jacky <jacky123789456@gmail.com>
2022-05-06 17:45:32 +0800

    下拉菜單ok

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="dropdown">
-    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle">{{ title }}</a>
-    <ul class="dropdown-menu" :style="{ display: 'block' }">
-        <li class="dropdown-item">
-            <a href="#">新建文章</a>
-        </li>
-        <li class="dropdown-item">
-            <a href="#">編輯資料</a>
-        </li>
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{ title }}</a>
+    <ul class="dropdown-menu" :style="{ display: 'block' }" v-if="isOpen">
+      <li class="dropdown-item">
+        <a href="#">新建文章</a>
+      </li>
+      <li class="dropdown-item">
+        <a href="#">編輯資料</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'dropdown',
   props: {
     title: {
       type: String,
       required: true
+    }
+  },
+  setup () {
+    const isOpen = ref(false)
+
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    return {
+      isOpen, toggleOpen
     }
   }
 })
